4fae9fab7fb332d4ee489120314ddc68ae9ec51a
Arthur Fernandez <arthur@snooget.com>
2023-04-22 12:16:21 -0700

    Added the parent class with props

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+  - Below I will take my first shoot at the school catalouge project with no help.
+*/
+// This is the parent class for primary, middle, and highschool classes
+class School {
+  constructor(name) {
+    this._name = name;
+    this._level = ['primary', 'middle', 'high'];
+    this._numberOfStudents = numberOfStudents;
+  }
+}
